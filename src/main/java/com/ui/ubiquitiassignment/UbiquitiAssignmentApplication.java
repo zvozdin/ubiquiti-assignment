258d624eb2bdbe0937f05796ca2fc546784f39de
Oleksandr Zvozdin <zvyozdin@gmail.com>
2024-07-17 19:53:12 +0200

    init

---
+++ src/main/java/com/ui/ubiquitiassignment/UbiquitiAssignmentApplication.java
@@ -0,0 +1,13 @@
+package com.ui.ubiquitiassignment;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class UbiquitiAssignmentApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(UbiquitiAssignmentApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/ui/ubiquitiassignment/UbiquitiAssignmentApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ui.ubiquitiassignment;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class UbiquitiAssignmentApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
